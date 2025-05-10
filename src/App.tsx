e5d9e179eec72ebf597136a96020646f0f560ffd
Rafael Couto Silva <rafacsilva016@gmail.com>
2025-05-10 16:03:03 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './reset.css'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+       
+      </Routes>
+    </Router>
+  )
+}
+
+export default App

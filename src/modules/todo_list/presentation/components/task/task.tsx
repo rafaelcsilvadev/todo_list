dc92b7df05a020e6846b7fd5412439328e468dbb
Rafael Couto Silva <rafacsilva016@gmail.com>
2025-05-24 18:42:03 -0300

    Created  todo list page and adjusted some components

--- src/modules/todo_list/presentation/components/task/task.tsx
+++ src/modules/todo_list/presentation/components/task/task.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import style from "./task.module.css";
+import Button from "../button/button";
 
 interface TaskProps {
     readonly id?: string;
@@ -7,18 +8,27 @@ interface TaskProps {
     readonly value?: string;
     readonly label?: string;
     readonly description?: string;
+    readonly checked?: boolean;
+    readonly onDelete?: () => void;
 }
 
 
 function Task({ ...props }: TaskProps): ReactNode {
     return (
         <div className={style.checkBox}>
-            <input id={props.id} type="checkbox" name={props.name} value={props.value} />
-            <div>
-                <label htmlFor={props.id}>{props.label}</label>
-                <span>
-                    {props.description}
-                </span>
+            <input id={props.id} type="checkbox" name={props.name} value={props.value} checked={props.checked} />
+            <div className="inside">
+                <div className="descriptions">
+                    <label htmlFor={props.id}>{props.label}</label>
+                    <span>
+                        {props.description}
+                    </span>
+                </div>
+                <div className="buttons">
+                    <Button label="Delete" onClick={() => { }} />
+                    <div />
+                    <Button label="Update" onClick={() => { }} />
+                </div>
             </div>
         </div>
     );
--- src/modules/todo_list/presentation/pages/todo_list/todo_list_page.tsx
+++ src/modules/todo_list/presentation/pages/todo_list/todo_list_page.tsx
@@ -1,9 +1,51 @@
 import type { ReactNode } from "react";
 import Nav from "../../components/nav/nav";
+import type { TodoListEntity } from "../../../domain/entities/todo_list_entity";
+import Task from "../../components/task/task";
+import style from "./todo_list.module.css";
+import Button from "../../components/button/button";
+
+const mock: TodoListEntity[] = [
+    {
+        id: 1,
+        title: "Todo 1411111111111111111",
+        description: "Description 1",
+        completed: false,
+        createdAt: new Date("2023-01-01"),
+    },
+    {
+        id: 2,
+        title: "Todo 2",
+        description: "Description 2",
+        completed: true,
+        createdAt: new Date("2023-01-02"),
+    },
+]
 
 function TodoListPage(): ReactNode {
     return (
-        <Nav title="teste"/>
+        <>
+            <Nav title="Todo List" />
+            <main className={style.main}>
+                {mock.map((todo) => (
+                    <>
+                        <br />
+                        <Task
+                            key={todo.id}
+                            id={todo.id.toString()} description={todo.description}
+                            label={todo.title}
+                            checked={todo.completed}
+                        />
+                    </>
+
+                ))}
+                <div className={style.buttonBox}> 
+                    <Button label="New Task"
+                        onClick={() => { }} 
+                        />
+                </div>
+            </main>
+        </>
     )
 }
 
